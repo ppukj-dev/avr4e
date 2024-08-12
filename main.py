6d924b7bfbfcef90b5aec9dad9080495ef38e655
rusdiy <rusdiy.afkar@gmail.com>
2024-08-12 16:22:00 +0900

    fix reference error

--- main.py
+++ main.py
@@ -159,7 +159,9 @@ async def add_sheet(ctx, url=""):
         actions_data['MaxUsages'].replace('', 0, inplace=True)
         actions_data['Usages'].replace('', 0, inplace=True)
         actions_data.replace('#REF!', None, inplace=True)
-        actions_data = actions_data.dropna()
+        actions_data.dropna(inplace=True)
+        df_data.replace('#REF!', None, inplace=True)
+        df_data.dropna(inplace=True)
 
         name = df_data[df_data['field_name'] == 'Name']['value'].iloc[0]
         charaRepo = CharacterUserMapRepository()
@@ -195,6 +197,10 @@ async def update_sheet(ctx, url=""):
         # clean empty cells
         actions_data['MaxUsages'].replace('', 0, inplace=True)
         actions_data['Usages'].replace('', 0, inplace=True)
+        actions_data.replace('#REF!', None, inplace=True)
+        actions_data.dropna(inplace=True)
+        df_data.replace('#REF!', None, inplace=True)
+        df_data.dropna(inplace=True)
 
         name = df_data[df_data['field_name'] == 'Name']['value'].iloc[0]
         charaRepo = CharacterUserMapRepository()
