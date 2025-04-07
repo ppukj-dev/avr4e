ef105b23508f25ecd9a5923df20deaf61df1486c
Rusdiy Afkar <rusdiy.afkar@gmail.com>
2025-04-07 15:58:29 +0900

    add repo

--- repository.py
+++ repository.py
@@ -98,3 +98,81 @@ class CharacterUserMapRepository(Repository):
         with self as db:
             db.cursor.execute(query, (data, actions, id))
             db.connection.commit()
+
+
+class GachaMapRepository(Repository):
+    def __init__(self):
+        super().__init__()
+        self.connection = sqlite3.connect("database/avr4e.db")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS gacha_map (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            guild_id VARCHAR(255) NOT NULL,
+            start TEXT NOT NULL,
+            items TEXT NOT NULL,
+            sheet_url TEXT NOT NULL,
+            UNIQUE (guild_id)
+        )""")
+        self.cursor.close()
+        self.connection.close()
+
+    def get_gacha(self, guild_id: str) -> tuple:
+        query = """
+        SELECT
+            id,
+            guild_id,
+            start,
+            items,
+            sheet_url
+        FROM gacha_map
+        WHERE guild_id = ?
+        LIMIT 1
+        """
+
+        with self as db:
+            db.cursor.execute(query, (guild_id,))
+            result = db.cursor.fetchone()
+
+        return result
+
+    def set_gacha(
+            self,
+            guild_id: str,
+            start: str,
+            items: str,
+            sheet_url: str
+            ) -> None:
+        query = """
+        INSERT INTO gacha_map (
+            guild_id, start, items, sheet_url
+        )
+        VALUES (
+            ?, ?, ?, ?
+        )
+        ON CONFLICT (guild_id)
+            DO UPDATE SET
+                start = ?,
+                items = ?,
+                sheet_url = ?
+        """
+
+        with self as db:
+            db.cursor.execute(query, (
+                guild_id,
+                start, items, sheet_url,
+                start, items, sheet_url))
+            db.connection.commit()
+
+    def update_character(self, id: int, start: str, items: str) -> None:
+        query = """
+        UPDATE gacha_map
+        SET
+            start = COALESCE(?, start),
+            items = COALESCE(?, items)
+        WHERE id = ?
+        """
+
+        with self as db:
+            db.cursor.execute(query, (start, items, id))
+            db.connection.commit()
