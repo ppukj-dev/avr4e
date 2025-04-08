304e340f610fefbff96325d19f11b941b56e8ed8
Rusdiy Afkar <rusdiy.afkar@gmail.com>
2025-04-09 00:52:30 +0900

    fix env issue

--- repository.py
+++ repository.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASS = os.getenv("MYSQL_PASS")
-MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_DB = "db_4e"
 
 
 class Repository:
